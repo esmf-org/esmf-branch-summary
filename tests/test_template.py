1977ae58969b3a5573936de5de48fd3f69722ade
Ryan Long <ryan@saltycatfish.com>
2021-12-13 12:43:47 -0500

    Add critical tests.

--- tests/test_template.py
+++ tests/test_template.py
@@ -1,7 +1,48 @@
 # pylint: disable=all
-import esmf_branch_summary
+import esmf_branch_summary as ebs
+
+from unittest.mock import patch
+
+
+class Dummy(object):
+    pass
 
 
 def test():
-    assert "function" in str(type(esmf_branch_summary.main))
-    assert True
+    assert "function" in str(type(ebs.main))
+
+
+def testFindFilesContainingString_whenCalledWithFoundValue_ReturnsCorrectFiles():
+    expected = 2
+    actual = len(
+        ebs.find_files_containing_string(
+            "ESMF_8_3_0_beta_snapshot_04-8-g60a38ef",
+            ".\\tests\\fixtures\\summary_data_files",
+        )
+    )
+    assert actual == expected
+
+
+def testFindFilesContainingString_whenCalledWithNotFoundValue_ReturnsCorrectFiles():
+    expected = 0
+    actual = len(
+        ebs.find_files_containing_string(
+            "non_existant_hash_123",
+            ".\\tests\\fixtures\\summary_data_files",
+        )
+    )
+    assert actual == expected
+
+
+@patch("subprocess.run")
+def testGetLastBranchHash_whenCalled_ReturnsLastBranchHash(run_mock):
+    log = ""
+    with open(".\\tests\\fixtures\\git_log\\git_log_sample.log") as _file:
+        log = _file.read()
+
+    o = Dummy()
+    o.stdout = log
+    run_mock.return_value = o
+    expected = "ESMF_8_3_0_beta_snapshot_04-8-g60a38ef"
+    actual = ebs.get_last_branch_hash("develop", "cheyenne")
+    assert expected == actual
