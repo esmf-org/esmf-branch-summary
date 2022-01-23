4e35ab1ad9a10cd045553339668382d29fbc49c9
Ryan Long <ryanlong1004@gmail.com>
2022-01-22 22:11:06 -0500

    Add db gateway.

---
+++ gateway.py
@@ -0,0 +1,38 @@
+import sqlite3
+from collections import namedtuple
+from typing import List
+
+
+SummaryRow = namedtuple(
+    "SummaryRow",
+    "branch, host, compiler_type, compiler_version, mpi_type, mpi_version, o_g, os, unit_pass, unit_fail, system_pass, system_fail, example_pass, example_fail, nuopc_pass, nuopc_fail, build_passed, hash, modified, id",
+)
+
+
+class Archive:
+    def __init__(self, db_path):
+        self.con = sqlite3.connect(db_path)
+
+    def create_table(self):
+        cur = self.con.cursor()
+        cur.execute(
+            """CREATE TABLE if not exists Summaries (branch, host, compiler_type, compiler_version, mpi_type, mpi_version, o_g, os, unit_pass, unit_fail, system_pass, system_fail, example_pass, example_fail, nuopc_pass, nuopc_fail, build_passed, hash, modified DATETIME DEFAULT CURRENT_TIMESTAMP, id PRIMARY KEY)"""
+        )
+        cur.execute("""CREATE INDEX if not exists summary_id_idx ON Summaries (id)""")
+        self.con.commit()
+
+    def insert_rows(self, rows: List[SummaryRow]):
+        cur = self.con.cursor()
+        cur.executemany(
+            "INSERT OR REPLACE INTO summaries VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+            rows,
+        )
+        self.con.commit()
+
+    def fetch_rows_by_hash(self, _hash: str):
+        cur = self.con.cursor()
+        cur.execute(
+            """SELECT branch, host, compiler_type, compiler_version, mpi_type, mpi_version, o_g, os, unit_pass, unit_fail, system_pass, system_fail, example_pass, example_fail, nuopc_pass, nuopc_fail, build_passed, hash, modified, id FROM Summaries WHERE hash = ?""",
+            (_hash,),
+        )
+        return (SummaryRow(*item) for item in cur.fetchall())
