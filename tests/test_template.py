38901bd31cc403447e43fda3c220f90facb47776
Ryan Long <ryanlong1004@gmail.com>
2022-02-10 16:59:38 -0500

    Update and test qty.

--- tests/test_template.py
+++ tests/test_template.py
@@ -31,20 +31,6 @@ def test_fetch_branches():
         "ESMF_8_3_0_beta_snapshot_06-7-gdaedd10",
         "ESMF_8_3_0_beta_snapshot_06-1-ga2344ba",
         "ESMF_8_3_0_beta_snapshot_05-30-gf84ebe0",
-        "ESMF_8_3_0_beta_snapshot_05-28-g1022def",
-        "ESMF_8_3_0_beta_snapshot_05-4-gcfea0e5",
-        "ESMF_8_3_0_beta_snapshot_05-3-g889e709",
-        "ESMF_8_3_0_beta_snapshot_05-2-geb61916",
-        "ESMF_8_3_0_beta_snapshot_05-1-g3e9d170",
-        "ESMF_8_3_0_beta_snapshot_04-8-g60a38ef",
-        "ESMF_8_3_0_beta_snapshot_04-7-gc7b07b1",
-        "ESMF_8_3_0_beta_snapshot_03-5-g5fdc9bc",
-        "ESMF_8_3_0_beta_snapshot_02-9-g35b0f61",
-        "ESMF_8_3_0_beta_snapshot_02-3-gbcb5d21",
-        "ESMF_8_3_0_beta_snapshot_02-1-gd594fca",
-        "ESMF_8_3_0_beta_snapshot_00-1-ge61c3b7",
-        "ESMF_8_2_0_beta_snapshot_20-18-g0aeed17",
-        "ESMF_8_2_0_beta_snapshot_20-1-gb0d63f0",
     ]
 
     actual = job.get_branch_hashes(_job, _git)
