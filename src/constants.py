029e026cb059d2bf33154343b288da13a16bf518
Ryan Long <ryan.long@noaa.gov>
2022-02-28 01:15:48 -0500

    change constants for prod

--- src/constants.py
+++ src/constants.py
@@ -10,7 +10,7 @@ DEFAULT_FILE_ENCODING = "ISO-8859-1"
 DEFAULT_TEMP_SPACE_NAME = "esmf_branch_summary_space"
 
 # Repositories
-SUMMARIES_REPO = "git@github.com:ryanlong1004/esmf-test-summary.git"
+SUMMARIES_REPO = "git@github.com:esmf/esmf-test-summary.git"
 
 # Machines
 MACHINE_NAME_LIST = sorted(
