c2c546426c035922e7a9fd3f91f2ed62a419fd64
Ryan Long <ryan.long@noaa.gov>
2022-03-02 20:57:42 -0500

    force commit log

--- update_repos.py
+++ update_repos.py
@@ -68,7 +68,7 @@ def main():
         Git(_path).pull()
     summary_repo = Git(pathlib.Path(".").resolve())
     summary_repo.add(LOG_FILE_PATH)
-    summary_repo.commit(f"update {LOG_FILE_PATH}")
+    summary_repo.commit(f"update {LOG_FILE_PATH}", force=True)
     summary_repo.push()
 
     logging.info("finished")
