a02a1274d325e405ecf451341e6fc3c45fcee5e0
Ryan Long <ryanlong1004@gmail.com>
2022-03-15 15:57:56 -0400

    add debug

--- src/job/processor.py
+++ src/job/processor.py
@@ -315,6 +315,7 @@ class Processor:
         self.gateway.git_artifacts.pull()
 
         for idx, _hash in enumerate(self.get_recent_branch_hashes(job)):
+            logging.debug(idx, _hash)
             summary = self.generate_summary(_hash, job)
             if len(summary) == 0:
                 logging.info(
